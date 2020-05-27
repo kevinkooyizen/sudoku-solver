34d6b09336864fec12b027b9c49f7016d66c85a3
Kevin Koo <kooyizen@hotmail.com>
2020-05-27 11:24:28 +0800

    setup initial css for sudoku grid

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,90 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+  <main>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li v-for="cell in cells">
+        <span>{{ cell }}</span>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="note">Your browser doesn't support CSS Grid. You'll need <a href="http://gridbyexample.com/browsers/">a browser that does</a> to use this app.</div>
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data () {
+      return {
+        cells: [],
+      }
+    },
+    computed: {
+
+    },
+    created() {
+      for(let i = 1; i <= 81; i++) {
+        this.cells.push('');
+      }
     }
   }
-}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  body{
+    font-family: Avenir, Heveltica, sans-serif;
+  }
+  li:nth-child(n):nth-child(-n+9) {
+    border-top-width: 4px;
+  }
+  li:nth-child(n+73):nth-child(-n+81) {
+    border-bottom-width: 4px;
+  }
+  li:nth-child(3n) {
+    border-right-width: 4px;
+  }
+  li:nth-child(9n+1) {
+    border-left-width: 4px;
+  }
+  li:nth-child(n+19):nth-child(-n+27) {
+    border-bottom-width: 4px;
+  }
+  li:nth-child(n+46):nth-child(-n+54) {
+    border-bottom-width: 4px;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  ul {
+    display: grid;
+    grid-template-columns: repeat(9, 5vw);
+    grid-template-rows: repeat(9, 5vw);
+    justify-content: center;
+    align-content: center;
+    grid-gap: 0rem;
+    list-style: none;
+    margin: 0 0 2vw;
+    padding: 0;
+    font-size: calc(2vw + 10px);
+  }
+  li {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    border: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  span {
+    margin-top: 0.4rem;
+  }
 
-a {
-  color: #42b983;
-}
+  .note {
+    background: #ddd;
+    font-family: monospace;
+    padding: 2em 5em;
+    font-size: 120%;
+    order: -1;
+  }
+  @supports (display:grid) {
+    .note {display:none;}
+  }
 </style>
